7ee95249462ca878b83ff7a5943fde35f6e97a86
Maksym <maximkack@gmail.com>
2020-09-16 16:09:47 +0300

    [+] The testng.xml run has been implemented!

---
+++ src/main/java/tests/SingletonChrome.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SingletonChrome extends WebDriverOptions {
+
+    private static SingletonChrome instance = null;
+    private WebDriver driver;
+
+    private SingletonChrome() {
+    }
+
+    public WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "web-drivers/chromedriver");
+        driver = new ChromeDriver(getChromeOptions());
+        return driver;
+    }
+
+    public static SingletonChrome initDriver() {
+        if (instance == null)
+            instance = new SingletonChrome();
+        return instance;
+    }
+}
