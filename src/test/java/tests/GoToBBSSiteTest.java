1f7da71f7f0643f539e6b7363c7b51b8ad775a92
Maksym <maximkack@gmail.com>
2020-09-17 12:12:29 +0300

    [+] Added elements present test.

--- src/test/java/tests/GoToBBSSiteTest.java
+++ src/test/java/tests/GoToBBSSiteTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import basetest.BaseTest;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 import static basetest.BbsConstants.baseUrl;
@@ -8,13 +9,20 @@ import static basetest.BbsConstants.baseUrl;
 public class GoToBBSSiteTest extends BaseTest {
 
     @Test
+    public void testBBSSiteElementsPresent() {
+        BbcMainPage bbsPageObject1 = new BbcMainPage(driver);
+        Assert.assertTrue(bbsPageObject1.searchTextForm.isEnabled());
+        Assert.assertTrue(bbsPageObject1.searchTextForm.isDisplayed());
+    }
+
+    @Test
     public void testBBSSite() {
         driver.get(baseUrl);
         String searchWord = "dog";
-        BbcMainPage bbsPageObject = new BbcMainPage(driver);
+        BbcMainPage bbsPageObject2 = new BbcMainPage(driver);
 
-        bbsPageObject.sendTextIntoSearchTextForm(searchWord);
-        bbsPageObject.searchButtonClick();
-        bbsPageObject.clickOnLink();
+        bbsPageObject2.sendTextIntoSearchTextForm(searchWord);
+        bbsPageObject2.searchButtonClick();
+        bbsPageObject2.clickOnLink();
     }
 }
